64f1f601a9d00cb708525d545c9a3651bb78da30
Katherine <katherine@kaytea.dev>
2025-02-14 01:34:10 -0500

    feat: eww tweaks, discord colorscheme

--- eggs/eww/scripts/sunset.sh
+++ eggs/eww/scripts/sunset.sh
@@ -17,16 +17,16 @@ status="--status"
 toggle="--toggle"
 
 if [[ $arg = $toggle ]]; then
-    pgrep hyprsunset &> /dev/null
+    pgrep wlsunset &> /dev/null
     state=$?
     if [[ $state -eq 0 ]];then
-        pkill hyprsunset
+        pkill wlsunset
     else
-        nohup hyprsunset -t 4000 &> /dev/null &
+        nohup wlsunset -t 4000 &> /dev/null &
     fi
 elif [[ $arg = $status ]]; then
     while sleep 0.5; do
-        pgrep hyprsunset &> /dev/null
+        pgrep wlsunset &> /dev/null
         echo $?
     done
 else
--- eggs/zsh/.zprofile
+++ eggs/zsh/.zprofile
@@ -1,3 +1,3 @@
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-    exec niri --session
+    dbus-run-session niri --session >> "$HOME/.cache/niri-logs.txt"
 fi
